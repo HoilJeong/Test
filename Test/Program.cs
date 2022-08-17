2cdd5b102d3c51664e62c7a558346c0b33fc28b5
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-08-17 23:30:09 +0900

    초기 커밋.

---
+++ Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    using System;
+
+    public class Solution
+    {
+        public static int[,] array = new int[2, 3] { {1, 2 ,3 }, { 4, 5, 6 } };
+        
+
+        public static int solution(int[,] sizes)
+        {
+            int answer = 0;
+
+            int w = 0;
+            int h = 0;
+
+            for (int i = 0; i < sizes.GetLength(0); i++)
+            {
+                int maxW = Math.Max(sizes[i, 0], sizes[i, 1]);
+                int maxH = Math.Min(sizes[i, 0], sizes[i, 1]);
+
+                w = Math.Max(w, maxW);
+                h = Math.Max(h, maxH);
+            }
+
+            answer = w * h;
+
+            return answer;
+        }
+
+        public static void Main()
+        {           
+            Console.WriteLine(solution(array));
+        }
+    }
+}
